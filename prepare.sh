cd903d4d78dc26bfea611cd5ba507733735d58e1
Volker Gaibler <volker.gaibler@h-its.org>
2017-02-15 15:19:40 +0100

    "prepare.sh" should do a fresh build
    Need to remove traces from previous builds

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 #
-# compile Bonsai
+# compile Bonsai freshly
 
-mkdir -p runtime/build
-cd runtime/build
+builddir=runtime/build
+echo "Deleting old build..."
+rm -fr $builddir
+echo "Now building freshly..."
+mkdir -p $builddir
+cd $builddir
 cmake -DUSE_MPI:BOOL=OFF -DUSE_OPENGL:BOOL=ON -DWAR_OF_GALAXIES:BOOL=ON -DUSE_THRUST:BOOL=ON ../
 make
-
+echo "Done."
